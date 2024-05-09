54d6d8c91965620cf8777c7e560984ae2071b1eb
PepeVm <pepe_vm2@hotmail.com>
2024-05-08 22:15:47 -0600

    PP

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -11,12 +11,15 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-          backgroundColor:const Color.fromARGB(255, 33, 1, 95),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
           foregroundColor: Colors.white,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
-      child: Text(text, textAlign: TextAlign.center,),
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/models/questions.dart
+++ lib/models/questions.dart
@@ -2,54 +2,55 @@ import 'package:quiz_app/models/quiz_question.dart';
 
 const questions = [
   QuizQuestion(
-    'What are the main building blocks of Flutter UIs?',
+    '¿Cómo se le llama a la planta que come insectos y los utiliza para fertilizarse?',
     [
-      'Widgets',
-      'Components',
-      'Blocks',
-      'Functions',
+      'Carnívora()',
+      'Venus()',
+      'Atrapamoscas()',
+      'Todas las anteriores()',
     ],
   ),
-  QuizQuestion('How are Flutter UIs built?', [
-    'By combining widgets in code',
-    'By combining widgets in a visual editor',
-    'By defining widgets in config files',
-    'By using XCode for iOS and Android Studio for Android',
+  QuizQuestion(
+      '¿La sábila de qué planta es famosa por sus propiedades curativas?', [
+    'Aloe Vera()',
+    'Agave()',
+    'Maguey()',
+    'Echeveria()',
   ]),
   QuizQuestion(
-    'What\'s the purpose of a StatefulWidget?',
+    '¿Cuál de los siguientes materiales NO puedes usar para tu composta?',
     [
-      'Update UI as data changes',
-      'Update data as UI changes',
-      'Ignore data changes',
-      'Render UI that does not depend on data',
+      'Cáscara de naranja()',
+      'Café mólido()',
+      'Bolsa de papitas()',
+      'Corazón de manzana()',
     ],
   ),
   QuizQuestion(
-    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    '¿Qué clima es el mejor para sembrar un mango?',
     [
-      'StatelessWidget',
-      'StatefulWidget',
-      'Both are equally good',
-      'None of the above',
+      'Tropical()',
+      'Polar()',
+      'Desértico()',
+      'Todos los anteriores()',
     ],
   ),
   QuizQuestion(
-    'What happens if you change data in a StatelessWidget?',
+    'Las suculentas son famosas por almacenar una gran cantidad de agua, por lo tanto, la mayoría de ellas...',
     [
-      'The UI is not updated',
-      'The UI is updated',
-      'The closest StatefulWidget is updated',
-      'Any nested StatefulWidgets are updated',
+      'El riego debe de ser constante, porque necesitan mucha agua()',
+      'El riego debe de ser normal, porque ya se encargan de almacenar el agua que necesitan()',
+      'El riego debe de ser poco frecuente, ya que se encargan de almacenar el agua necesaria()',
+      'No se riegan y pueden sobrevivir con la humedad del ambiente()',
     ],
   ),
   QuizQuestion(
-    'How should you update data inside of StatefulWidgets?',
+    '¿Por qué mi cactus o suculenta se estira anormalmente sin ningún motivo aparente',
     [
-      'By calling setState()',
-      'By calling updateData()',
-      'By calling updateUI()',
-      'By calling updateState()',
+      'Por falta de agua()',
+      'Por falta de luz()',
+      'Porque le hablo feo()',
+      'Porque está enferma()',
     ],
   ),
 ];
